5c8051aa35356002e5ab9a06dac3bd1378b547bc
askanicash <saeedkhanbaloch040@gmail.com>
2026-06-07 13:25:49 +0500

    Add points management and withdrawal request functions
    This script manages points for a task claiming system and allows users to withdraw points via a messaging service.

---
+++ script.js
@@ -0,0 +1,38 @@
+let points = Number(localStorage.getItem("points")) || 0;
+
+document.getElementById("points").innerText = points;
+
+function claimTask(reward){
+points += reward;
+
+localStorage.setItem("points", points);
+
+document.getElementById("points").innerText = points;
+
+alert("+" + reward + " Points Added");
+}
+
+function withdrawRequest(){
+
+if(points < 100){
+alert("Minimum 100 Points Required");
+return;
+}
+
+let method =
+document.getElementById("method").value;
+
+let account =
+document.getElementById("account").value;
+
+let msg =
+"Withdrawal Request%0A" +
+"Method: " + method + "%0A" +
+"Account: " + account + "%0A" +
+"Points: " + points;
+
+window.open(
+"https://wa.me/YOURNUMBER?text=" + msg,
+"_blank"
+);
+}
